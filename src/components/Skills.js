cdff19593781832b80a0e30b230f1db6f9936370
ollatomiwa <ajaomoshood@gmail.com>
2025-01-25 14:02:16 +0100

    Team component and integrate into App; fix container class in Skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,7 @@ import { skills } from '../data';
 const Skills = () => {
   return (
     <section id='skills' className='section bg-[#f8f3eca1] pb-32'>
-        <div className='containermx-auto text-center'>
+        <div className='container mx-auto text-center'>
             <h2 className='text-5xl font-primary font-extrabold mb-4'>What we do</h2>
             <p className='max-w-[540px] mx-auto px-6 lg:px-0 mb-[60px]'>
                 lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
