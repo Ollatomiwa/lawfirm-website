3850e60b0ff08d741f8b8e085cafd32589aa428a
ollatomiwa <ajaomoshood@gmail.com>
2025-01-25 01:40:00 +0100

    Testimonials component and integrate testimonial slider into App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 // import components
 import Header from './components/Header';
 import Banner from './components/Banner';
-import Appointment from './components/Appointment';
+import Testimonials from './components/Testimonials';
 
 const App = () => {
   return <div>
     <Header />
     <Banner />
+    <Testimonials />
     
     <div className='h-[2000px]'></div>
   </div>;
