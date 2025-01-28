4f28b1c2e018d0f318a0f53cc6d23c0ebec20fd7
Tomiwa <ajaomoshood@gmail.com>
2025-01-28 13:12:43 +0100

    minor

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,12 +21,12 @@ const Header = ()=> {
         });
     });
 
-    return <header className={`${bg && 'bg-white shadow-md py-2'} p-8 lg:px-0 w-full fixed z-10 
+    return <header className={`${bg && 'bg-white shadow-md py-4'} p-8 lg:px-0 w-full fixed z-10 
         transition-all duration-300`}
         >
-            <div className='container mx-auto flex items-center'>
+            <div className='container mx-auto flex items-center justify-between'>
                 {/* logo */}
-                <a href='#' className='justify-start'>
+                <a href='#'>
                     <img src={Logo} alt='' />
                 </a>
                 {/* Nav */}
